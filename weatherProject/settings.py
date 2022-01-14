02bfcfa350ea4f1598c5edd62b3cebe39006c3c1
Shamim Sikder <35293225+shamimsikder@users.noreply.github.com>
2022-01-14 13:21:06 +0600

    Fixing some problem

--- weatherProject/settings.py
+++ weatherProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3p5&)=dgw8o)hz*)+qgba!blk-)7z-v*7r6_s8^luwi2!2g+z('
+SECRET_KEY = '' #Inser your own secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
